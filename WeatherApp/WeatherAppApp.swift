7a0bfa801e99b5987aa7852b07691ae011f767b1
John Trujillo <johnandrestrujillo@gmail.com>
2024-11-13 08:39:05 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by John Trujillo on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
